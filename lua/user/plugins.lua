a2de1193f437d3f50002cbd2d2f593f7dadc024f
over-vlad <romanovich.vo@gmail.com>
2024-05-05 18:34:32 +0200

    init commit

---
+++ lua/user/plugins.lua
@@ -0,0 +1,23 @@
+lvim.plugins = {
+  {
+    "ruifm/gitlinker.nvim",
+    event = "BufRead",
+    desc = "Copy shareable file permalinks",
+    config = function()
+      require("gitlinker").setup {
+        opts = {
+          -- remote = 'github', -- force the use of a specific remote
+          -- adds current line nr in the url for normal mode
+          add_current_line_on_normal_mode = true,
+          -- callback for what to do with the url
+          action_callback = require("gitlinker.actions").copy_to_clipboard,
+          -- print the url after performing the action
+          print_url = true,
+          -- mapping to call url generation
+          mappings = "<leader>gy",
+        },
+      }
+    end,
+    dependencies = "nvim-lua/plenary.nvim",
+  },
+}
